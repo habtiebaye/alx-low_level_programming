ad73ea24c8576dcfb3bf8127d9f877a67bc846bc
habtiebaye <habtie1080@gmail.com>
2022-09-10 02:41:14 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,88 +2,90 @@
 
 /**
  *
- *  * * main - main block
+ * * main - main block
  *
- *   * * Print all possible different combinations of 3 digits.
+ * * Print all possible different combinations of 3 digits.
  *
- *    * * Numbers must be separated by commas and a space.
+ * * Numbers must be separated by commas and a space.
  *
- *     * * The 3 digits must be different.
+ * * The 3 digits must be different.
  *
- *      * * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * * 012, 120, 102, 021, 201, 210 are considered the same combination.
  *
- *       * * print only the smallest combination of 3 digits.
+ * * print only the smallest combination of 3 digits.
  *
- *        * * Numbers should be printed in ascending order.
+ * * Numbers should be printed in ascending order.
  *
- *         * * You can only use `putchar` to print to console.
+ * * You can only use `putchar` to print to console.
  *
- *          * * You can only use `putchar` up to 6 times.
+ * * You can only use `putchar` up to 6 times.
  *
- *           * * You are not allowed to use any variables of type `char`.
+ * * You are not allowed to use any variables of type `char`.
  *
- *            * * Return: 0
+ * * Return: 0
  *
- *             * *
+ * *
  *
- *              */
+ * *
+ *
+ */
 
 int main(void)
 
 {
 
-		int a, b, c, d;
+	int a, b, c, d;
 
 
 
-			for (a = 0; a < 1000; a++)
+	for (a = 0; a < 1000; a++)
 
 
 
-					{
+	{
 
-								b = a / 100; /* hundreds */
+		b = a / 100; /* hundreds */
 
-										c = (a / 10) % 10; /* tens */
+		c = (a / 10) % 10; /* tens */
 
-												d = a % 10; /* singles */
-
-
-
-														if (b < c && c < d)
+		d = a % 10; /* singles */
 
 
 
-																	{
-
-																					putchar(b + '0');
-
-																								putchar(c + '0');
-
-																											putchar(d + '0');
+		if (b < c && c < d)
 
 
 
-																														if (a < 700)
+		{
+
+			putchar(b + '0');
+
+			putchar(c + '0');
+
+			putchar(d + '0');
 
 
 
-																																		{
-
-																																							putchar(',');
-
-																																											putchar(' ');
-
-																																														}
-
-																																}
-
-															}
-
-				putchar('\n');
+			if (a < 700)
 
 
 
-					return (0);
+			{
+
+				putchar(',');
+
+				putchar(' ');
+
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+
+
+
+	return (0);
 
 }
