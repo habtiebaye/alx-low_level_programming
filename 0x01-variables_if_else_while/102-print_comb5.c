866a2ff882458e2d9523fe17cd70431905c0e566
habtiebaye <habtie1080@gmail.com>
2022-09-10 02:33:18 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 /**
- *
  * * main - Prints 3 combination of numbers
  *
  * *
@@ -16,74 +14,68 @@ int main(void)
 
 {
 
-		int c, i, k, j;
+	int c, i, k, j;
 
+	for (c = 48; c <= 57; c++)
 
+	{
 
-			for (c = 48; c <= 57; c++)
+		for (i = 48; i <= 57; i++)
+
+		{
+
+			for (k = 48; k <= 57; k++)
+
+			{
+
+				for (j = 48; j <= 57; j++)
+
+				{
+
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
 
 					{
 
-								for (i = 48; i <= 57; i++)
+						putchar(c);
 
-											{
+						putchar(i);
 
-															for (k = 48; k <= 57; k++)
+						putchar(' ');
 
-																			{
+						putchar(k);
 
-																								for (j = 48; j <= 57; j++)
+						putchar(j);
 
-																													{
+						if (c + i + k + j == 227 && c == 57)
 
-																																			if (((k + j) > (c + i) &&  k >= c) || c < k)
+						{
 
-																																									{
+							break;
 
-																																																putchar(c);
+						}
 
-																																																						putchar(i);
+						else
 
-																																																												putchar(' ');
+						{
 
-																																																																		putchar(k);
+							putchar(',');
 
-																																																																								putchar(j);
+							putchar(' ');
 
+						}
 
+					}
 
-																																																																													if (c + i + k + j == 227 && c == 57)
+				}
 
-																																																																																			{
+			}
 
-																																																																																									break;
+		}
 
-																																																																																														}
+	}
 
-																																																																																		else
+	putchar('\n');
 
-																																																																																								{
-
-																																																																																														putchar(',');
-
-																																																																																																			putchar(' ');
-
-																																																																																																								}
-
-																																																																																							}
-
-																																							}
-
-																											}
-
-																	}
-
-									}
-
-
-
-				putchar('\n');
-
-				return (0);
+	return (0);
 
 }
