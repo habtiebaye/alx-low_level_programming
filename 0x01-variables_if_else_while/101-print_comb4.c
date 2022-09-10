20fad8a13a71aa9f030bfe18c1c33be51a7a0c36
habtiebaye <habtie1080@gmail.com>
2022-09-10 02:25:25 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,58 +8,60 @@
  *
  * * Return: Always (Success)
  *
- * *
- *
  */
 
 int main(void)
 
 {
 
-	int c, i, k;
+		int c, i, k;
 
-	for (c = '0'; c <= '9'; c++)
-
-	{
-
-		for (i = '0'; i <= '9'; i++)
-
-		{
-
-			for (k = '0'; k <= '9'; k++)
-
-			{
-
-				if (c < i && i < k)
-
-				{
-
-					putchar(c);
-
-					putchar(i);
-
-					putchar(k);
-
-					if (c != '7')
+			for (c = '0'; c <= '9'; c++)
 
 					{
 
-						putchar(',');
+								for (i = '0'; i <= '9'; i++)
 
-						putchar(' ');
+											{
 
-					}
+															for (k = '0'; k <= '9'; k++)
 
-				}
+																			{
 
-			}
+																								if (c < i && i < k)
 
-		}
+																													{
 
-	}
+																																			putchar(c);
 
-	putchar('\n');
+																																								putchar(i);
 
-	return (0);
+																																													putchar(k);
+
+
+
+																																																		if (c != '7')
+
+																																																								{
+
+																																																															putchar(',');
+
+																																																																					putchar(' ');
+
+																																																																										}
+
+																																																						}
+
+																											}
+
+																	}
+
+									}
+
+				putchar('\n');
+
+					return (0);
 
 }
+
+
