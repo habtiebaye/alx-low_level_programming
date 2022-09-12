435f1dd1f312d03845f48108c94c6643bf32d641
habtiebaye <habtie1080@gmail.com>
2022-09-12 06:45:03 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * * main - main block
  *
@@ -32,45 +31,25 @@
 int main(void)
 
 {
-
-	int a, b, c, d;
-
-
-
+  int a, b, c, d;
 	for (a = 0; a < 1000; a++)
-
-
-
 	{
-
 		b = a / 100; /* hundreds */
 
 		c = (a / 10) % 10; /* tens */
 
 		d = a % 10; /* singles */
-
-
-
 		if (b < c && c < d)
-
-
-
 		{
-
 			putchar(b + '0');
 
 			putchar(c + '0');
 
 			putchar(d + '0');
 
-
-
 			if (a < 700)
 
-
-
 			{
-
 				putchar(',');
 				putchar(' ');
 		}
