364c62844720f27db19a3b19b21ac591f01dd8d2
habtiebaye <habtie1080@gmail.com>
2022-09-13 07:23:53 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  *   * _islower - Checks for lowercase character
  *
  *     * @c: The character to be checked
@@ -12,7 +11,7 @@
  *
  *         * Return: 1 for lowercase character or 0 for anything else
  *
- *           */
+ */
 
 int _islower(int c)
 
