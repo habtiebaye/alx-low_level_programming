6b410749e3d22ab1787da4f6cefd70f9707f6862
habtiebaye <habtie1080@gmail.com>
2022-09-13 23:44:54 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,10 +9,9 @@
  *         * Return: 1 for lowercase character or 0 for anything else
  *
  */
-
-int _islower(int c)  
+int _islower(int c)
 {
-if (c >= 97 && c <= 122)    
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
