b0e21181b3cda5e74f167d8ae48e922060bfa392
habtiebaye <habtie1080@gmail.com>
2022-09-15 07:36:36 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,17 +9,17 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b && b > c)
+if (a >= b && a >= c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b >= a && b >= c)
 {
 largest = b;
 }
 else
 {
-larges;
+largest = c;
 }
 return (largest);
 }
