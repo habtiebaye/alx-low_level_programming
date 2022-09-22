da5960c6e2e16a72f588f73586225f0c649a6170
habtiebaye <habtie1080@gmail.com>
2022-09-22 00:27:15 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,18 @@
 #include "main.h"
 /**
+ *
  * * _strncat - Concatenates two strings
+ *
  * * @dest: The destination value
+ *
  * * @src: The source value
+ *
  * * @n: The limit of the concatenation
+ *
+ * *
+ *
  * * Return: A pointer to the resulting string dest
+ *
  * */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -21,5 +29,4 @@ j++;
 }
 dest[dlen + n + 1] = '\0';
 return (dest);
-int _putchar(char c);
 }
