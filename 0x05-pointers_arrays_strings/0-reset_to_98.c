f95b1eaf391eddcb8a9b3f9485895e0e494c08b8
habtiebaye <habtie1080@gmail.com>
2022-10-05 05:29:15 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,43 +1,10 @@
-#include "notrebloh.h"
+#include  "notrebloh.h"
 
 /**
- *
- * * _strlen - return length of a string
- *
- * *
- *
- * * @s: char type
- *
- * * Return:  length of string
- *
+ * reset_to_98 - updates values
+ *@n: int type
  */
-int _strlen(char *s)
+void reset_to_98(int *n)
 {
-int a;
-for (a = 0; s[a] != '\0'; a++)
-{
-}
-return (a);
-}
-
-/**
- *
- * * _strcat - function to concatnate strings
- *
- * * @dest: destination for concatnation
- *
- * * @src: source of string
- *
- * * Return: dest
- *
- */
-char *_strcat(char *dest, char *src)
-{
-int dest_len;
-int a;
-dest_len = _strlen(dest);
-for (a = 0 ; src[a] != '\0' ; a++)
-dest[dest_len + a] = src[a];
-dest[dest_len + a] = '\0';
-return (dest);
+*n = 98;
 }
